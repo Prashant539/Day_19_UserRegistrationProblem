7f27b28831b86deeb9ea608577668795b6f49406
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-20 17:16:20 +0530

    [Prashant]: User enter a vaild last name

---
+++ RegexPatternLastName/LastName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPattern
+{
+    public class LastName
+    {
+        public void UserRegistration()
+        {
+            Regex Lastname = new Regex("^[A-Z]{1}[a-z]{9}$");
+            Console.WriteLine("Enter the last name: ");
+            string LastName = Console.ReadLine();
+            if (Lastname.IsMatch(LastName))
+            {
+                Console.WriteLine("your name is: " + LastName);
+
+            }
+            else
+            {
+                Console.WriteLine("your entered last name is incorrect pttern");
+            }
+        }
+    }
+}
---
+++ RegexPatternLastName/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace RegexPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LastName lastName  = new LastName();
+            lastName.UserRegistration();
+
+        }
+    }
+}
