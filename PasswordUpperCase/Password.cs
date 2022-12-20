004788dc18b1775a6b1aca1e92ba971e0eeb98c3
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-20 23:38:16 +0530

    [Prashant]:Password with at least one upper case

---
+++ PasswordUpperCase/Password.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PasswordUpperCase
+{
+    public class Password
+    {
+        public static string REGEX_PasswordUpper = "^[A-Z]{1}[a-z]{2,8}$";
+        public bool validatePasswordUpper(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PasswordUpper);
+        }
+           
+    }
+}
---
+++ PasswordUpperCase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PasswordUpperCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Password password = new Password();
+            Console.WriteLine(password.validatePasswordUpper("Prashant"));
+        }
+    }
+}
