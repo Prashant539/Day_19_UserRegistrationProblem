3b89aad692777d2723ac1250334571442da49e8e
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-20 17:22:18 +0530

    [Prashant]: User need to enter vaild email

---
+++ Email/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace RegexPatternMatchingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidEmail validemail = new ValidEmail();
+            Console.WriteLine(validemail.validateEmail("abc.xzy@bl.co.in"));
+        }
+    }
+}
---
+++ Email/ValidEmail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPatternMatchingDemo
+{
+    public class ValidEmail
+    {
+        public static string REGEX_Email = ("^[a-z]{3}.[a-z]{3}@(bl).(co).(in)$");
+        public bool validateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_Email);
+        }
+
+    }
+}
