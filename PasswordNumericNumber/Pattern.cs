f17355a6148cd1e35672855115e32214617396e2
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-20 23:40:46 +0530

    [Prashant]:Password with at least one numeric no

---
+++ PasswordNumericNumber/Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PasswordNumericNumber
+{
+    public class Pattern
+    {
+        public static string REGEX_PASSWORDNUMBER = "^[A-Z]{1}[a-z]{2,8}[0-9]{1}$";
+        public bool validatePasswordNumber(string passwordNumber)
+        {
+            return Regex.IsMatch(passwordNumber, REGEX_PASSWORDNUMBER);
+
+        }
+    }
+}
---
+++ PasswordNumericNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PasswordNumericNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine(pattern.validatePasswordNumber("Prashant5"));
+        }
+    }
+}
