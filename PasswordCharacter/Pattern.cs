b4aa892db80686b0bd8b7ef49090c6dbc1d5da99
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-20 23:43:02 +0530

    [Prashant]:Password with one special character

---
+++ PasswordCharacter/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PasswordCharacter
+{
+    public class Pattern
+    {
+        public static string REGEX_PASSWORD1SPECIALCHAR = "^[A-Z]{1}[a-z]{2,8}[0-9]{3,5}[@]$";
+        public bool validatePassword1SpecialChar(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD1SPECIALCHAR);
+        }
+    }
+}
---
+++ PasswordCharacter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PasswordCharacter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine(pattern.validatePassword1SpecialChar("Prashant805@"));
+        }
+    }
+}
