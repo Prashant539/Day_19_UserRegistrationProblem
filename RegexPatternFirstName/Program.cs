cb2131e98e55ee560c93bf9464c6e1faae43b4c8
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-20 17:14:07 +0530

    [Prashant]: Enter a valid first name

---
+++ RegexPatternFirstName/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace RegexPatternMatching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program");
+            UserFirstName userFirstName = new UserFirstName();
+            userFirstName.UserRegistration();
+        }
+    }
+}
---
+++ RegexPatternFirstName/UserFirstName.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPatternMatching
+{
+    internal class UserFirstName
+    {
+        public void UserRegistration()
+        {
+            Regex Firstname = new Regex("^[A-Z]{1}[a-z]{7}$");
+            Console.WriteLine("Enter FirstName: ");
+            string FirstName = Console.ReadLine();
+            if (Firstname.IsMatch(FirstName))
+            {
+                Console.WriteLine("your name is: " + FirstName);
+                    
+
+
+            }
+            else
+            {
+                Console.WriteLine("your entered name is invalid pattern");
+            }
+        }
+    }
+}
