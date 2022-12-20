382cca71046cf00444efff08381a36119c8f05ed
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-20 17:33:56 +0530

    [Prashant]:User need pre-defind mobile number

---
+++ UserMobileNumber/MobileNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserMobileNumber
+{
+    public class MobileNumber
+    {
+        public static String REGEX_MobileNumber = "^[0-9]{2}[ ][0-9]{10}$";
+        public bool validateMobileNumber(string mobileNumber)
+        {
+            return Regex.IsMatch(mobileNumber, REGEX_MobileNumber);
+        }
+    }
+}
---
+++ UserMobileNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace UserMobileNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MobileNumber mobileNumber = new MobileNumber();
+            Console.WriteLine(mobileNumber.validateMobileNumber("91 9838084421"));
+        }
+    }
+}
